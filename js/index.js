e5358e3d0fae22fb63ca40ef5460e6cc7aae938a
Manuel Alva <barrera_ml@hotmail.com>
2024-04-19 01:08:41 -0600

    Finished Bonus Task

--- js/index.js
+++ js/index.js
@@ -1,192 +1,132 @@
 import { getRequest, postRequest, deleteRequest } from "./api/requests";
+import {
+  albumtemplate,
+  favoriteAlbumTemplate,
+  defaultAlbumTemplate,
+} from "./templates/album";
 
-const url = "https://661a03c6125e9bb9f29b2c25.mockapi.io/api/v1/albums";
+const albumsUrl = "https://661a03c6125e9bb9f29b2c25.mockapi.io/api/v1/albums";
+const favoriteAlbumsUrl =
+  "https://661a03c6125e9bb9f29b2c25.mockapi.io/api/v1/favorites";
+
 const favoritesTab = document.querySelector("#favorites-tab");
 const searchTab = document.querySelector("#search-tab");
-const searchButtton = document.querySelector("#search-button");
-const favoritesButtton = document.querySelector("#favorites-button");
+const searchButton = document.querySelector("#search-button");
+const favoritesButton = document.querySelector("#favorites-button");
 const searchForm = document.querySelector("#search-form");
 const query = document.querySelector("#query");
 const invalidInput = document.querySelector("#invalid-input");
 const albumsList = document.querySelector("#albums-list");
 const messageContainer = document.querySelector("#message-container");
 const feedbackContainer = document.querySelector("#feedback-container");
-const btnAddFavourites = document.querySelector("#btn-add-favorites");
+const btnAddFavorites = document.querySelector("#btn-add-favorites");
 const myAlbums = document.querySelector("#my-albums");
 
-//fetch album data
-async function fetchAlbumData() {
-  const albumsData = await getRequest(url);
+let albumStore = [];
+let favoriteAlbumStore = [];
 
-  return albumsData;
+// Fetch album data  when the application starts
+async function fetchAlbumData() {
+  try {
+    albumStore = await getRequest(albumsUrl);
+  } catch (error) {
+    console.error("Error fetching album data:", error);
+  }
 }
 
 async function fetchFavoriteAlbums() {
-  const favoriteAlbumsData = await getRequest(
-    "https://661a03c6125e9bb9f29b2c25.mockapi.io/api/v1/favorites"
-  );
-  return favoriteAlbumsData;
-}
-
-//create fetched albums data backup
-const albumStore = await fetchAlbumData();
-
-//create fetched favorite albums data backup
-const favoriteAlbumStore = await fetchFavoriteAlbums();
-
-console.log(favoriteAlbumStore);
-
-/*TASK 1*/
-
-//Switch to Favorites Tab
-favoritesButtton.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  //Render favorite albums
-  renderFavouriteAlbums();
-
-  favoritesTab.classList.remove("d-none");
-  searchTab.classList.add("d-none");
-
-  searchButtton.classList.remove("active");
-  favoritesButtton.classList.add("active");
-
-  removeAlbum();
-});
-
-//Switch to Search Tab
-searchButtton.addEventListener("click", (event) => {
-  event.preventDefault();
-  searchTab.classList.remove("d-none");
-  favoritesTab.classList.add("d-none");
-
-  searchButtton.classList.add("active");
-  favoritesButtton.classList.remove("active");
-});
-
-/*TASK 2*/
-searchForm.addEventListener("submit", onSubmitAlbumSearch);
-query.addEventListener("input", onHandleQueryAlbumInput);
-
-function onSubmitAlbumSearch(e) {
-  e.preventDefault();
-  isSuccess();
-}
-
-//Validate input fields
-function validateInputFields() {
-  const albumInput = query.value.toLowerCase().trim();
-
-  if (albumInput === "") {
-    invalidInput.classList.add("d-flex");
-    invalidInput.classList.remove("d-none");
-  } else {
-    invalidInput.classList.remove("d-flex");
-    invalidInput.classList.add("d-none");
-  }
-  return albumInput;
-}
-
-//Handle warnings on input event
-function onHandleQueryAlbumInput() {
-  if (albumInput.value) {
-    invalidInput.classList.remove("d-flex");
-    invalidInput.classList.add("d-none");
-  }
-}
-
-//search for the album title/artist name using filter function
-function searchAlbumOrArtist(searchCriteria) {
-  if (!searchCriteria) {
-    return [];
-  }
-  const query = searchCriteria;
-
-  const results = albumStore.filter((album) => {
-    if (album.albumName.toLowerCase().includes(query)) {
-      return album;
-    }
-    if (album.artistName.toLowerCase().includes(query)) {
-      return album;
-    }
-  });
-  if (results.length === 0) {
-    displaySearchNullMessage();
-  }
-  return results;
-}
-
-//Render search results
-function renderAlbumSearch(searchCriteria) {
-  const container = albumsList.cloneNode(true);
-
   try {
-    container.innerHTML = ""; //Clear container
-
-    if (searchCriteria.length > 0) {
-      searchCriteria.forEach(
-        ({ albumName, averageRating, artistName, uid }) => {
-          const albumtemplate = `
-        <li
-        class="list-group-item d-flex justify-content-between align-items-start"
-      >
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">
-            ${albumName}
-            <span class="badge bg-primary rounded-pill">${averageRating}</span>
-          </div>
-          <span>${artistName}</span>
-        </div>
-        <button data-uid="${uid}" type="button" class="btn btn-success btn-add-favorites">
-          Add to Favorites
-        </button>
-      </li>`;
-          container.insertAdjacentHTML("afterbegin", albumtemplate);
-        }
-      );
-      document.querySelector("#albums-list").replaceWith(container);
-    }
+    favoriteAlbumStore = await getRequest(favoriteAlbumsUrl);
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching album data:", error);
   }
 }
 
-function isSuccess() {
-  const albumlInput = validateInputFields();
+fetchAlbumData(); //load album data
+fetchFavoriteAlbums(); //load favorites data
 
-  let albumResults = [];
+/* TASK 1 */
 
-  //filter only by album or artist
-  if (albumlInput) {
-    messageContainer.innerHTML = ""; //Clear null search msg if present
+// Switch to Favorites Tab
+favoritesButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  renderFavouriteAlbums(myFavorites);
+  switchToTab(favoritesTab);
+  removeAlbum(); //Initialize the deleteRequest
+});
 
-    albumResults = searchAlbumOrArtist(albumlInput);
-    renderAlbumSearch(albumResults);
+// Switch to Search Tab
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  switchToTab(searchTab);
+});
 
-    const buttons = document.querySelectorAll(".btn-add-favorites");
+function switchToTab(tab) {
+  favoritesTab.classList.add("d-none");
+  searchTab.classList.add("d-none");
+  tab.classList.remove("d-none");
+}
 
-    buttons.forEach((button) => {
-      button.addEventListener("click", (event) => {
-        let albumUid = event.target.dataset.uid;
-        addFavouriteAlbums(albumUid);
-      });
+/* TASK 2 */
+searchForm.addEventListener("submit", onSubmitAlbumSearch);
+async function onSubmitAlbumSearch(e) {
+  e.preventDefault();
+  const albumInput = query.value.toLowerCase().trim();
+  if (albumInput === "") {
+    displayInputError();
+  } else {
+    messageContainer.innerHTML = ""; // Clear previous messages
+    const searchResults = await searchAlbumOrArtist(albumInput);
+    renderAlbumSearch(searchResults);
+  }
+  renderButtons();
+}
+
+function displayInputError() {
+  invalidInput.classList.add("d-flex");
+  invalidInput.classList.remove("d-none");
+}
+
+async function searchAlbumOrArtist(albumSearch) {
+  const query = albumSearch;
+  return albumStore.filter(
+    (album) =>
+      album.albumName.toLowerCase().includes(query) ||
+      album.artistName.toLowerCase().includes(query)
+  );
+}
+
+function renderAlbumSearch(searchResults) {
+  albumsList.innerHTML = ""; // Clear previous search results
+  if (searchResults.length === 0) {
+    displaySearchNullMessage();
+  } else {
+    searchResults.forEach(({ albumName, averageRating, artistName, uid }) => {
+      albumsList.insertAdjacentHTML(
+        "beforeend",
+        albumtemplate({ albumName, averageRating, artistName, uid })
+      );
     });
   }
 }
 
 function displaySearchNullMessage() {
-  const displayNullSearchMsg = `
-  <div>
-    <h2>No albums found!!</h2>
-  </div>
-          `;
-  messageContainer.innerHTML = displayNullSearchMsg;
+  messageContainer.innerHTML = `<div><h2>No albums found!!</h2></div>`;
 }
 
 /* Task 3 */
+const renderButtons = () => {
+  document.querySelectorAll(".btn-add-favorites").forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      const albumUid = event.target.dataset.uid;
+      await addFavouriteAlbums(albumUid);
+    });
+  });
+};
+
 let myFavorites = [];
 
-function addFavouriteAlbums(albumUid) {
+async function addFavouriteAlbums(albumUid) {
   const album = albumStore.find((album) => album.uid === albumUid);
 
   if (album && !myFavorites.some((favAlbum) => favAlbum.uid === albumUid)) {
@@ -211,51 +151,15 @@ function createFeedbackMessage() {
 /*TASK 4*/
 
 //Render favourite albums when changing tabs
-function renderFavouriteAlbums() {
-  const container = myAlbums.cloneNode(true);
-  try {
-    container.innerHTML = ""; //Clear container
+function renderFavouriteAlbums(listOfAlbums) {
+  myAlbums.innerHTML = ""; //Clear container
 
-    if (myFavorites.length > 0) {
-      myFavorites.forEach(({ albumName, averageRating, artistName, uid }) => {
-        const albumtemplate = `
-        <li data-uid="${uid}"
-        class="list-group-item d-flex justify-content-between align-items-start"
-      >
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">
-            ${albumName}
-            <span class="badge bg-primary rounded-pill">${averageRating}</span>
-          </div>
-          <span>${artistName}</span>
-        </div>
-        <button data-uid="${uid}" type="button" class="btn btn-success btn-remove-favorites">
-        Remove from Favorites
-        </button>
-      </li>`;
-        container.insertAdjacentHTML("afterbegin", albumtemplate);
-      });
-      document.querySelector("#my-albums").replaceWith(container);
-    } else {
-      myAlbums.innerHTML += `
-      <li
-      class="list-group-item d-flex justify-content-between align-items-start"
-    >
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">
-          OK Computer
-          <span class="badge bg-primary rounded-pill">4.23</span>
-        </div>
-        <span>Radiohead</span>
-      </div>
-      <button type="button" class="btn btn-success">
-        Remove from Favorites
-      </button>
-    </li>
-      `;
-    }
-  } catch (error) {
-    console.error(error);
+  if (listOfAlbums.length > 0) {
+    listOfAlbums.forEach((album) => {
+      myAlbums.insertAdjacentHTML("beforeend", favoriteAlbumTemplate(album));
+    });
+  } else {
+    myAlbums.innerHTML += defaultAlbumTemplate;
   }
 }
 
@@ -277,9 +181,29 @@ function removeAlbum() {
   });
 }
 
-function removeFavouriteAlbums(albumUid) {
-  const album = favoriteAlbumStore.find((album) => album.uid === albumUid);
-  deleteRequest(album.id);
-  myFavorites.filter((album) => album.uid === albumUid);
-  console.log("Removed from favorites:", album);
+async function removeFavouriteAlbums(albumUid) {
+  try {
+    await fetchFavoriteAlbums();
+    const albumIndex = favoriteAlbumStore.findIndex(
+      (album) => album.uid === albumUid
+    );
+
+    if (albumIndex !== -1) {
+      const removedAlbum = favoriteAlbumStore.splice(albumIndex, 1)[0]; // Remove the album from favoriteAlbumStore
+      await deleteRequest(removedAlbum.id); // Delete the album from the API
+      myFavorites.splice(albumIndex, 1)[0];
+
+      // Remove the album from the DOM
+      const albumElement = myAlbums.querySelector(`[data-uid="${albumUid}"]`);
+      if (albumElement) {
+        albumElement.remove();
+      }
+
+      console.log("Removed from favorites:", removedAlbum.albumName);
+    } else {
+      console.log("Album not found in favorites:", albumIndex);
+    }
+  } catch (error) {
+    console.error("Error removing favorite album:", error);
+  }
 }
--- js/templates/album.js
+++ js/templates/album.js
@@ -1,15 +1,64 @@
-const albumtemplate = `
-        <li
-        class="list-group-item d-flex justify-content-between align-items-start"
-      >
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">
-            ${album.albumName}
-            <span class="badge bg-primary rounded-pill">${album.averageRating}</span>
-          </div>
-          <span>${album.artistName}</span>
-        </div>
-        <button data-uid="${album.uid}" type="button" class="btn btn-success btn-add-favorites">
-          Add to Favorites
-        </button>
-      </li>`;
+export const albumtemplate = ({
+  albumName,
+  averageRating,
+  artistName,
+  uid,
+}) => `
+<li data-uid="${uid}"
+class="list-group-item d-flex justify-content-between align-items-start"
+>
+<div class="ms-2 me-auto">
+  <div class="fw-bold">
+    ${albumName}
+    <span class="badge bg-primary rounded-pill">${averageRating}</span>
+  </div>
+  <span>${artistName}</span>
+</div>
+<button data-uid="${uid}" type="button" class="btn btn-success btn-add-favorites">
+Add to Favorites
+</button>
+</li>`;
+
+export const favoriteAlbumTemplate = ({
+  albumName,
+  averageRating,
+  artistName,
+  uid,
+}) => `
+<li data-uid="${uid}"
+class="list-group-item d-flex justify-content-between align-items-start"
+>
+<div class="ms-2 me-auto">
+  <div class="fw-bold">
+    ${albumName}
+    <span class="badge bg-primary rounded-pill">${averageRating}</span>
+  </div>
+  <span>${artistName}</span>
+</div>
+<button data-uid="${uid}" type="button" class="btn btn-success btn-remove-favorites">
+Remove from Favorites
+</button>
+</li>`;
+
+// Define your default album template
+export const defaultAlbumTemplate = `
+<li
+class="list-group-item d-flex justify-content-between align-items-start"
+>
+<div class="ms-2 me-auto">
+  <div class="fw-bold">
+    Name of the Album
+    <span class="badge bg-primary rounded-pill">4.23</span>
+  </div>
+  <span>Artist</span>
+</div>
+<button
+  data-uid="uid"
+  id=""
+  type="button"
+  class="btn btn-success"
+>
+Remove from Favorites
+</button>
+</li>>
+`;
